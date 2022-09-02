d601d53b7fd01456e6ff5fcd9a32a941405bc44e
xingyu.hu <xingyu.hu@shopee.com>
2022-09-02 14:59:33 +0800

    first

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "./src/index.tsx",
+  plugins: [
+    typescript({
+      declaration: true
+    })
+  ],
+  output: [
+    {
+      format: "es",
+      file: `dist/index.esm.js`,
+      sourcemap: false
+    },
+    {
+      format: "cjs",
+      file: `dist/index.cjs.js`,
+      sourcemap: false
+    }
+  ]
+};
